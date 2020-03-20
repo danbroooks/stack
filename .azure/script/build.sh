1d42bc0e9e253c867830c5443382ee58e0321fe3
Jeff Happily <goodboy96@live.com.my>
2020-03-20 10:57:10 +0800

    test

--- .azure/script/build.sh
+++ .azure/script/build.sh
@@ -1,16 +1,16 @@
-set -x
+# set -x
 
 pwd
 ls
 
-echo $1
-echo $2
+# echo $1
+# echo $2
 
-os_name=$1
-dir=$2
+# os_name=$1
+# dir=$2
 
-echo $os_name
-echo $dir
+# echo $os_name
+# echo $dir
 
 
 # export STACK_ROOT="$(Build.SourcesDirectory)"/.stack-root;
