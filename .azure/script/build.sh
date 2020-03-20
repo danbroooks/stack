153276fdcbe061d9023448c97ca74efc42864284
Jeff Happily <goodboy96@live.com.my>
2020-03-20 12:06:52 +0800

    Add build script

--- .azure/script/build.sh
+++ .azure/script/build.sh
@@ -1,41 +1,56 @@
-# set -x
+#!/bin/bash
+set -x
 
 os_name=$1
 dir=$2
+cache_s3_version=$3
+cache_s3_prefix=$4
+base_branch=$5
+source_branch=$6
 
-echo $os_name
-echo $dir
 
+# Setup isolated environment
+mkdir -p $dir/{bin,lib64,lib}
+mkdir -p ${dir}${HOME}
 
-# export STACK_ROOT="$(Build.SourcesDirectory)"/.stack-root;
-# echo $STACK_ROOT
-# mkdir -p ~/.local/bin
-# curl -f -L "https://github.com/fpco/cache-s3/releases/download/${CACHE_S3_VERSION}/cache-s3-${CACHE_S3_VERSION}-linux-x86_64.tar.gz" -o ~/.local/bin/cache-s3.tar.gz
-# tar xzf ~/.local/bin/cache-s3.tar.gz -C ~/.local/bin
-# export PATH=$HOME/.local/bin:$PATH;
-# cache-s3 --prefix="${CACHE_S3_PREFIX}" --git-branch="$(Build.SourceBranchName)" --suffix="${OS_NAME}" restore stack --base-branch="${BASE_BRANCH}"
-# cache-s3 --prefix="${CACHE_S3_PREFIX}" --git-branch="$(Build.SourceBranchName)" --suffix="${OS_NAME}" restore stack work --base-branch="${BASE_BRANCH}"
-# etc/scripts/ci-setup.sh
-# export PATH=$HOME/.local/bin:/opt/ghc/$GHCVER/bin:/opt/happy/1.19.5/bin:/opt/alex/3.1.7/bin:$PATH
+cp -v /bin/{bash,ls, tar} $dir/bin
 
-# export STACK_ROOT="$(Build.SourcesDirectory)"/.stack-root;
-# export PATH=$HOME/.local/bin:$PATH;
-# set -ex
-# stack etc/scripts/release.hs build
-# cp _release/stack-* $(Build.ArtifactStagingDirectory)
-# set +ex
+list="$(ldd /bin/ls | egrep -o '/lib.*\.[0-9]')"
+for i in $list; do cp  -v "$i" "${dir}${i}"; done
 
-# - task: PublishBuildArtifacts@1
-# inputs:
-# pathtoPublish: '$(Build.ArtifactStagingDirectory)'
-# artifactName: stack-alpine-linux-64
-# condition: in(variables['Agent.JobStatus'], 'Succeeded', 'SucceededWithIssues', 'Failed')
-# - script: |
-# export PATH=$HOME/.local/bin:$PATH;
-# export AWS_ACCESS_KEY_ID="$(AWS_ACCESS_KEY_ID)";
-# export AWS_SECRET_ACCESS_KEY="$(AWS_SECRET_ACCESS_KEY)";
-# export STACK_ROOT="$(Build.SourcesDirectory)"/.stack-root;
-# if [ "$(Build.SourceBranchName)" = "${BASE_BRANCH}" ]; then
-# cache-s3 --prefix="${CACHE_S3_PREFIX}" --git-branch="$(Build.SourceBranchName)" --suffix="${OS_NAME}" save stack;
-# fi;
-# cache-s3 --prefix="${CACHE_S3_PREFIX}" --git-branch="$(Build.SourceBranchName)" --suffix="${OS_NAME}" save stack work
+list="$(ldd /bin/bash | egrep -o '/lib.*\.[0-9]')"
+for i in $list; do cp  -v "$i" "${dir}${i}"; done
+
+list="$(ldd /bin/tar | egrep -o '/lib.*\.[0-9]')"
+for i in $list; do cp  -v "$i" "${dir}${i}"; done
+
+# Build inside insolated environment
+cat << EOF | chroot $dir
+ori_stack_root=$STACK_ROOT
+ori_path=$PATH
+
+cd /stack
+export STACK_ROOT="$HOME/.stack-root;
+echo $STACK_ROOT
+mkdir -p $HOME/.local/bin
+curl -f -L "https://github.com/fpco/cache-s3/releases/download/${cache_s3_version}/cache-s3-${cache_s3_version}-linux-x86_64.tar.gz" -o $HOME/.local/bin/cache-s3.tar.gz
+tar xzf $HOME/.local/bin/cache-s3.tar.gz -C $HOME/.local/bin
+export PATH=$HOME/.local/bin:$PATH;
+cache-s3 --prefix="${cache_s3_prefix}" --git-branch="${source_branch}" --suffix="${os_name}" restore stack --base-branch="${base_branch}"
+cache-s3 --prefix="${cache_s3_prefix}" --git-branch="${source_branch}" --suffix="${os_name}" restore stack work --base-branch="${base_branch}"
+etc/scripts/ci-setup.sh
+export PATH=$HOME/.local/bin:/opt/ghc/$GHCVER/bin:/opt/happy/1.19.5/bin:/opt/alex/3.1.7/bin:$PATH
+
+set -ex
+stack etc/scripts/release.hs build
+set +ex
+
+if [ "${source_branch}" = "${base_branch}" ]; then
+    cache-s3 --prefix="${cache_s3_prefix}" --git-branch="${source_branch}" --suffix="${os_name}" save stack;
+fi;
+cache-s3 --prefix="${cache_s3_prefix}" --git-branch="${source_branch}" --suffix="${os_name}" save stack work
+
+# Reset env var
+export STACK_ROOT=$ori_stack_root
+export PATH=$ori_path
+EOF
